70412a5a2b79ad6382b7c17864ac3db3640c005d
Sirix1995 <arnaud.maurin@club-internet.fr>
2021-03-04 10:29:21 +0100

    Aborting normals
    PlantGL can compute them, and they seems to be incorrect in the file.

--- parser.py
+++ parser.py
@@ -44,7 +44,7 @@ class Parser():
 			"""print("Points : ", points)
 			print("Normales : ", normals)
 			print("Indices : ", indices)"""
-			lesMesh.append(TriangleSet(pointList = points, indexList= indices, normalList= normals))		
+			lesMesh.append(TriangleSet(pointList = points, indexList= indices))		
 		compteur = compteur + 1
 
 		for elt in elts:
