98535064b5e1d7d224a2642a9927d1608e3516ee
Sirix1995 <arnaud.maurin@club-internet.fr>
2021-04-22 16:59:44 +0200

    [GPUFLux Python] Preparing sample scene
    With a tetra mesh.

--- Essai_GPUflux_Python/test1.py
+++ Essai_GPUflux_Python/test1.py
@@ -1,6 +1,7 @@
 import sys
 import pyopencl as cl
 import numpy as np
+from openalea.plantgl.all import *  
 
 fichier = open("kernel/lightmodel_kernel.cl", "r")
 
@@ -46,6 +47,19 @@ queue = cl.CommandQueue(context)
 program = cl.Program(context, kernelSource).build(options)
 
 compute = program.compute
+
+# Scene
+points[ (0.0, 0.0, 0.0),
+        (0.0, 1.0, 0.0)
+        (1.0, 0.0, 0.0)
+        (1.0, 1.0, 1.0)]
+
+indices[(0, 1, 2)
+        (0, 1, 3)
+        (0, 2, 3)
+        (1, 2, 3)]
+
+tetra = TriangleSet(points, indices)
 
 # Params
 nbThread = 4
