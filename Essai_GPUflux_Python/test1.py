c96e4b3da8bb3be93d68a6dbe216e348d55643a2
Sirix1995 <arnaud.maurin@club-internet.fr>
2021-04-23 11:55:12 +0200

    [GPUFlux Python] Sample scene bugfix
    Fixing a definition problem.

--- Essai_GPUflux_Python/test1.py
+++ Essai_GPUflux_Python/test1.py
@@ -49,15 +49,15 @@ program = cl.Program(context, kernelSource).build(options)
 compute = program.compute
 
 # Scene
-points[ (0.0, 0.0, 0.0),
-        (0.0, 1.0, 0.0),
-        (1.0, 0.0, 0.0),
-        (1.0, 1.0, 1.0)]
+points = [(0.0, 0.0, 0.0),
+          (0.0, 1.0, 0.0),
+          (1.0, 0.0, 0.0),
+         (1.0, 1.0, 1.0)]
 
-indices[(0, 1, 2),
-        (0, 1, 3),
-        (0, 2, 3),
-        (1, 2, 3)]
+indices = [(0, 1, 2),
+           (0, 1, 3),
+           (0, 2, 3),
+           (1, 2, 3)]
 
 tetra = TriangleSet(points, indices)
 matrice = [(0.0, 0.0, 0.0, 0.0),]
