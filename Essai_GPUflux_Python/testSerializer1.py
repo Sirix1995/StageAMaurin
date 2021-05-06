b06230237c40c56f0a2372277c29c7576144e98f
Sirix1995 <arnaud.maurin@club-internet.fr>
2021-05-06 11:40:56 +0200

    [GPUFLux Python] First struct to char test
    Only returns a SegFault a this time...

---
+++ Essai_GPUflux_Python/testSerializer1.py
@@ -0,0 +1,39 @@
+import numpy as np
+import pyopencl as cl
+import pyopencl.tools
+import pyopencl.array
+
+context = cl.create_some_context(interactive=False)
+queue = cl.CommandQueue(context)
+structure = np.dtype([("entier", np.int32), ("flottant", np.float32)])
+print("Structure Python : ", structure)
+structure, structureCL = cl.tools.match_dtype_to_c_struct(context.devices[0], "structure", structure)
+print("Structure OpenCL : ", structureCL)
+
+table = np.empty(1, structure)
+table["entier"].fill(1)
+table["flottant"].fill(3.6)
+print("Table d'envoi : ", table)
+
+octets = table.tobytes()
+print("Table en Octets : ", octets)
+
+buffer = cl.Buffer(context, cl.mem_flags.READ_ONLY | cl.mem_flags.COPY_HOST_PTR, hostbuf=octets)
+
+kernelSource = structureCL + """
+                __kernel void vadd( __global char* donnees, int retour1, float retour2) {
+                        __global structure *params = (const __global structure*)donnees;
+                        params->entier+= 2;
+                        params->flottant+= 3.5;
+                        retour1 = params->entier;
+                        retour2 = params->flottant;
+                 }"""
+
+program = cl.Program(context, kernelSource).build()
+
+retour1 = 0
+retour2 = 0.0
+
+program.vadd(queue, (len(octets)), None, octets, retour1, retour2)
+print("Valeur de l'entier après exécution : ", retour1)
+print("Valeur du flottant après exécution : ", retour2)
