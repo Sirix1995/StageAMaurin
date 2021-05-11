a081d566724a50a4daf27030ae0c06339b1078b6
Sirix1995 <arnaud.maurin@club-internet.fr>
2021-05-11 16:27:25 +0200

    [GPUFLux Python] Putting options for kernel compilations
    Testing on the Xeon Machine.

--- Essai_GPUflux_Python/testSerializer2.py
+++ Essai_GPUflux_Python/testSerializer2.py
@@ -26,7 +26,37 @@ print("Structure Python : ", primitive)
 primitive, primitiveCL = cl.tools.match_dtype_to_c_struct(context.devices[0], "prim", primitive)
 print("Structure OpenCL : ", primitiveCL)
 
-options = " -I kernel/"
+# Options GPUFlux
+options = " -D MEASURE_FULL_SPECTRUM"
+options += " -D MEASURE_MIN_LAMBDA=380"
+options += " -D MEASURE_MAX_LAMBDA=720"
+options += " -D MEASURE_SPECTRUM_BINS=340"
+options += " -D SPECTRAL_WAVELENGTH_MIN=360"
+options += " -D SPECTRAL_WAVELENGTH_MAX=830"
+options += " -D SPECTRAL_WAVELENGTH_BINS=1"
+options += " -D BVH"
+options += " -D ENABLE_SENSORS"
+
+# Options machine
+options += " -D CL_KHR_GLOBAL_INT32_BASE_ATOMICS"
+options += " -D CL_KHR_GLOBAL_INT32_EXTENDED_ATOMICS"
+options += " -D CL_KHR_LOCAL_INT32_BASE_ATOMICS"
+options += " -D CL_KHR_LOCAL_INT32_EXTENDED_ATOMICS"
+options += " -D CL_KHR_FP64"
+options += " -D CL_KHR_BYTE_ADDRESSABLE_STORE"
+options += " -D CL_KHR_ICD"
+options += " -D CL_KHR_GL_SHARING"
+options += " -D CL_NV_COMPILER_OPTIONS"
+options += " -D CL_NV_DEVICE_ATTRIBUTE_QUERY"
+options += " -D CL_NV_PRAGMA_UNROLL"
+options += " -D CL_NV_COPY_OPTS"
+options += " -D CL_KHR_GL_EVENT"
+options += " -D CL_NV_CREATE_BUFFER"
+options += " -D CL_KHR_INT64_BASE_ATOMICS"
+options += " -D CL_KHR_INT64_EXTENDED_ATOMICS"
+
+# Options répertoire
+options += " -I kernel/"
 
 kernelSource = primitiveCL + """
                 #include "geo/prim.h"
