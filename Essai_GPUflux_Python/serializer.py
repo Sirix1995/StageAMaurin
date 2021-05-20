cafe1f835753de6ad15dbc5e765ba9423ae16cbd
Sirix1995 <arnaud.maurin@club-internet.fr>
2021-05-20 11:59:07 +0200

    [GPUFlux Python] Adding offsets in serializer
    Is returned by SerializeTriangleSet()

--- Essai_GPUflux_Python/serializer.py
+++ Essai_GPUflux_Python/serializer.py
@@ -44,86 +44,87 @@ class Serializer():
             resultList.append(triangle)
         return resultList
 
-    def serializePrimitive(self, dataTable, prim, primType, groupIndex, shaderOffset, indexOfReflexion):
+    # Put the generic infos of the primitive in the buffer
+    def setPrimInfos(self, buffer, prim, primType, groupIndex, shaderOffset, indexOfReflexion):
         #First Infos
-        dataTable["type"].fill(primType)
-        dataTable["groupIndex"].fill(groupIndex)
-        dataTable["shaderOffset"].fill(shaderOffset)
-        dataTable["indexOfReflexion"].fill(indexOfReflexion)
+        buffer["type"].fill(primType)
+        buffer["groupIndex"].fill(groupIndex)
+        buffer["shaderOffset"].fill(shaderOffset)
+        buffer["indexOfReflexion"].fill(indexOfReflexion)
 
         #BoundingBox
         bbox = BoundingBox(prim)
-        dataTable["xMin"].fill(bbox.getXMin())
-        dataTable["xMax"].fill(bbox.getXMax())
-        dataTable["yMin"].fill(bbox.getYMin())
-        dataTable["yMax"].fill(bbox.getYMax())
-        dataTable["zMin"].fill(bbox.getZMin())
-        dataTable["zMax"].fill(bbox.getZMax())
+        buffer["xMin"].fill(bbox.getXMin())
+        buffer["xMax"].fill(bbox.getXMax())
+        buffer["yMin"].fill(bbox.getYMin())
+        buffer["yMax"].fill(bbox.getYMax())
+        buffer["zMin"].fill(bbox.getZMin())
+        buffer["zMax"].fill(bbox.getZMax())
 
         #World to Object Matrix
-        dataTable["mat00"].fill(1.0)
-        dataTable["mat01"].fill(0.0)
-        dataTable["mat02"].fill(0.0)
-        dataTable["mat10"].fill(0.0)
-        dataTable["mat11"].fill(1.0)
-        dataTable["mat12"].fill(0.0)
-        dataTable["mat20"].fill(0.0)
-        dataTable["mat21"].fill(0.0)
-        dataTable["mat22"].fill(1.0)
-        dataTable["mat30"].fill(0.0)
-        dataTable["mat31"].fill(0.0)
-        dataTable["mat32"].fill(0.0)
+        buffer["mat00"].fill(1.0)
+        buffer["mat01"].fill(0.0)
+        buffer["mat02"].fill(0.0)
+        buffer["mat10"].fill(0.0)
+        buffer["mat11"].fill(1.0)
+        buffer["mat12"].fill(0.0)
+        buffer["mat20"].fill(0.0)
+        buffer["mat21"].fill(0.0)
+        buffer["mat22"].fill(1.0)
+        buffer["mat30"].fill(0.0)
+        buffer["mat31"].fill(0.0)
+        buffer["mat32"].fill(0.0)
 
     def serializeTriangle(self, triangle, groupIndex, shaderOffset, indexOfReflexion):
-        polygon = np.array(1, dtype= self.polygon)
-        self.serializePrimitive(polygon, triangle, 5, groupIndex, shaderOffset, indexOfReflexion)
+        buffer = np.array(1, dtype= self.polygon)
+        self.setPrimInfos(buffer, triangle, 5, groupIndex, shaderOffset, indexOfReflexion)
         normalVertex = triangle.normalList
 
         #Vertex coords
-        polygon["x1"].fill(triangle.pointAt(0)[0])
-        polygon["y1"].fill(triangle.pointAt(0)[1])
-        polygon["z1"].fill(triangle.pointAt(0)[2])
+        buffer["x1"].fill(triangle.pointAt(0)[0])
+        buffer["y1"].fill(triangle.pointAt(0)[1])
+        buffer["z1"].fill(triangle.pointAt(0)[2])
 
-        polygon["x2"].fill(triangle.pointAt(1)[0])
-        polygon["y2"].fill(triangle.pointAt(1)[1])
-        polygon["z2"].fill(triangle.pointAt(1)[2])
+        buffer["x2"].fill(triangle.pointAt(1)[0])
+        buffer["y2"].fill(triangle.pointAt(1)[1])
+        buffer["z2"].fill(triangle.pointAt(1)[2])
 
-        polygon["x3"].fill(triangle.pointAt(2)[0])
-        polygon["y3"].fill(triangle.pointAt(2)[1])
-        polygon["z3"].fill(triangle.pointAt(2)[2])
+        buffer["x3"].fill(triangle.pointAt(2)[0])
+        buffer["y3"].fill(triangle.pointAt(2)[1])
+        buffer["z3"].fill(triangle.pointAt(2)[2])
 
         #Face normal
         triangle.normelPerVertex = False
         triangle.computeNormalList()
 
-        polygon["faceNormalX"].fill(triangle.normalList[0][0])
-        polygon["faceNormalX"].fill(triangle.normalList[0][1])
-        polygon["faceNormalX"].fill(triangle.normalList[0][2])
+        buffer["faceNormalX"].fill(triangle.normalList[0][0])
+        buffer["faceNormalX"].fill(triangle.normalList[0][1])
+        buffer["faceNormalX"].fill(triangle.normalList[0][2])
 
         #UV coords
-        polygon["uvX1"].fill(0.0)
-        polygon["uvY1"].fill(0.0)
+        buffer["uvX1"].fill(0.0)
+        buffer["uvY1"].fill(0.0)
 
-        polygon["uvX2"].fill(0.0)
-        polygon["uvY2"].fill(0.0)
+        buffer["uvX2"].fill(0.0)
+        buffer["uvY2"].fill(0.0)
 
-        polygon["uvX3"].fill(0.0)
-        polygon["uvY3"].fill(0.0)
+        buffer["uvX3"].fill(0.0)
+        buffer["uvY3"].fill(0.0)
 
         #Vertex Normals
-        polygon["normalX1"].fill(normalVertex[0][0])
-        polygon["normalY1"].fill(normalVertex[0][1])
-        polygon["normalZ1"].fill(normalVertex[0][2])
+        buffer["normalX1"].fill(normalVertex[0][0])
+        buffer["normalY1"].fill(normalVertex[0][1])
+        buffer["normalZ1"].fill(normalVertex[0][2])
 
-        polygon["normalX2"].fill(normalVertex[0][0])
-        polygon["normalY2"].fill(normalVertex[0][1])
-        polygon["normalZ2"].fill(normalVertex[0][2])
+        buffer["normalX2"].fill(normalVertex[0][0])
+        buffer["normalY2"].fill(normalVertex[0][1])
+        buffer["normalZ2"].fill(normalVertex[0][2])
 
-        polygon["normalX3"].fill(normalVertex[0][0])
-        polygon["normalY3"].fill(normalVertex[0][1])
-        polygon["normalZ3"].fill(normalVertex[0][2])
+        buffer["normalX3"].fill(normalVertex[0][0])
+        buffer["normalY3"].fill(normalVertex[0][1])
+        buffer["normalZ3"].fill(normalVertex[0][2])
 
-        return polygon
+        return buffer
 
     def serializeTriangleSet(self, trSet, groupIndex, shaderOffset, indexOfReflexion):
         triangles = self.getTriangles(trSet)
@@ -133,10 +134,20 @@ class Serializer():
             triangleDataList.append(self.serializeTriangle(triangle, groupIndex, shaderOffset, indexOfReflexion))
 
         bytechain = triangleDataList[0].tobytes()
-        for triangleData in triangleDataList[1:]:
-            bytechain+= triangleData.tobytes()
 
-        return bytechain
+        offsets = np.empty(len(triangleDataList), np.int32)
+        offsetIndex = 1
+        offsets[0] = 0
+        acc = len(bytechain)
+
+        for triangleData in triangleDataList[1:]:
+            triangleInBytes = triangleData.tobytes()
+            bytechain+= triangleInBytes
+            offsets[offsetIndex] = acc + 1
+            offsetIndex+=1
+            acc+= len(triangleInBytes)
+
+        return bytechain, offsets
 
 serial = Serializer()
 
@@ -152,5 +163,52 @@ indices = [(0, 1, 2),
 
 tetra = TriangleSet(points, indices)
 
-resultat = serial.serializeTriangleSet(tetra, 0, 0, 0.0)
+resultat, offsets = serial.serializeTriangleSet(tetra, 0, 0, 0.0)
 print(resultat)
+print(offsets)
+
+# Options GPUFlux
+options = " -D MEASURE_FULL_SPECTRUM"
+options += " -D MEASURE_MIN_LAMBDA=380"
+options += " -D MEASURE_MAX_LAMBDA=720"
+options += " -D MEASURE_SPECTRUM_BINS=340"
+options += " -D SPECTRAL_WAVELENGTH_MIN=360"
+options += " -D SPECTRAL_WAVELENGTH_MAX=830"
+options += " -D SPECTRAL_WAVELENGTH_BINS=1"
+options += " -D BVH"
+options += " -D ENABLE_SENSORS"
+
+# Options machine
+options += " -D CL_KHR_GLOBAL_INT32_BASE_ATOMICS"
+options += " -D CL_KHR_GLOBAL_INT32_EXTENDED_ATOMICS"
+options += " -D CL_KHR_LOCAL_INT32_BASE_ATOMICS"
+options += " -D CL_KHR_LOCAL_INT32_EXTENDED_ATOMICS"
+options += " -D CL_KHR_FP64"
+options += " -D CL_KHR_BYTE_ADDRESSABLE_STORE"
+options += " -D CL_KHR_ICD"
+options += " -D CL_KHR_GL_SHARING"
+options += " -D CL_NV_COMPILER_OPTIONS"
+options += " -D CL_NV_DEVICE_ATTRIBUTE_QUERY"
+options += " -D CL_NV_PRAGMA_UNROLL"
+options += " -D CL_NV_COPY_OPTS"
+options += " -D CL_KHR_GL_EVENT"
+options += " -D CL_NV_CREATE_BUFFER"
+options += " -D CL_KHR_INT64_BASE_ATOMICS"
+options += " -D CL_KHR_INT64_EXTENDED_ATOMICS"
+
+# Options répertoire
+options += " -I kernel/"
+
+# Compilation du programme
+context = cl.create_some_context()
+queue = cl.CommandQueue(context)
+
+kernelSource =  """
+                #include "geo/prim.h"
+                
+                __kernel void structTest(__global char* donnees) {
+                    
+                 }"""
+
+
+program = cl.Program(context, kernelSource).build(options)
